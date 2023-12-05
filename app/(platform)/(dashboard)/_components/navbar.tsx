590e7b0c23df544cdab2ec4d6841c5209191bd04
Ryan Carr <ryan.g.carr@hotmail.com>
2023-12-04 21:08:33 -0500

    add organization navbar

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,23 @@
+import { Plus } from "lucide-react";
+
+import { Logo } from "@/components/logo";
+import { Button } from "@/components/ui/button";
+
+export function Navbar() {
+  return (
+    <nav className="fixed z-50 px-4 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+      {/* Need to add mobile sidebar */}
+      <div className="flex items-center gap-x-4">
+        <div className="hidden md:flex">
+          <Logo />
+        </div>
+        <Button size='sm' className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+          Create
+        </Button>
+        <Button size='sm' className="rounded-sm block md:hidden">
+          <Plus className="h-4 w-4" />
+        </Button>
+      </div>
+    </nav>
+  )
+}

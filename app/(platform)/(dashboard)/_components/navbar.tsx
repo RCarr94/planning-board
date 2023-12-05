3e54a4b3100956f2f5b5b73732dfd5c3bd994664
Ryan Carr <ryan.g.carr@hotmail.com>
2023-12-04 22:06:30 -0500

    fin login navbar on organization pg

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,6 +2,7 @@ import { Plus } from "lucide-react";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export function Navbar() {
   return (
@@ -11,12 +12,40 @@ export function Navbar() {
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button size='sm' className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+        <Button variant='primary' size='sm' className="rounded-sm hidden md:block h-auto py-1.5 px-2">
           Create
         </Button>
-        <Button size='sm' className="rounded-sm block md:hidden">
+        <Button variant='primary' size='sm' className="rounded-sm block md:hidden">
           <Plus className="h-4 w-4" />
         </Button>
+      </div>
+      <div className="ml-auto flex items-center gap-x-2">
+        <OrganizationSwitcher 
+          hidePersonal
+          afterCreateOrganizationUrl='/organization/:id'
+          afterLeaveOrganizationUrl="/select-org"
+          afterSelectOrganizationUrl='/organization/:id'
+          appearance={{
+            elements: {
+              rootBox: {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }
+            }
+          }}
+        />
+        <UserButton 
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: {
+                height: 30,
+                width: 30,
+              }
+            }
+          }}
+        />
       </div>
     </nav>
   )
